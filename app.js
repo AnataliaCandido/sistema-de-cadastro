2dfa012a8c4f66274284fb8053aafd3378456871
Anatalia <anatalialins@outlook.com>
2022-07-29 19:43:27 -0300

    retirando console

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
-console.log("rodando sistema de gerenciamento de jogos");
-console.log('mensagem');
-console.log('enviando para o github');
+console.log("rodando sistema de gerenciamento de jogos")
+console.log('mensagem')
