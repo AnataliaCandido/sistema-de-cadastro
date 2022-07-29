91e50ceb13fa63120ff073a16f5b2a3d5d0ec4d6
Anatalia <anatalialins@outlook.com>
2022-07-29 19:11:12 -0300

    adicionando um console.log

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-console.log("rodando sistema de gerenciamento de jogos")
-console.log('mensagem')
+console.log("rodando sistema de gerenciamento de jogos");
+console.log('mensagem');
+console.log('enviando para o github');
